37d4a705c7475d4bbd7f256240042550cb966485
lanyun1103 <4737557@qq.com>
2023-09-27 11:10:18 +0800

    build: refactor arch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  srcDir: 'src',
+
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@unocss/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@huntersofbook/naive-ui-nuxt',
+    '@nuxtjs/eslint-module',
+  ],
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        strict: true,
+        types: ['@pinia/nuxt', './type.d.ts'],
+      },
+    },
+  },
+  vite: {
+    logLevel: 'info',
+  },
+  devtools: { enabled: true },
 })
